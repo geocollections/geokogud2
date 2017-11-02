7b05ee7020a0f0da53341f3d085417749878c291
enmust <enmust@ttu.ee>
2017-11-02 18:53:07 +0200

    changed folder hierarchy

--- src/main/webapp/app/mainpage.js
+++ src/main/webapp/app/mainpage.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function ($) {
+killjQuery(document).ready(function ($) {
     "use strict"; // Start of use strict
 
    // jQuery for page scrolling feature - requires jQuery Easing plugin
