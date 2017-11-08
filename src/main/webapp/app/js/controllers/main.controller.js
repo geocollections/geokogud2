1b222b1740b70f0f796137b1ead83eef31e433c3
enmust <enmust@ttu.ee>
2017-11-08 17:29:43 +0200

    documented main.controller.js

--- src/main/webapp/app/js/controllers/main.controller.js
+++ src/main/webapp/app/js/controllers/main.controller.js
@@ -27,10 +27,14 @@ var constructor = function (configuration, $translate, $http, applicationService
     });
 
     function asyncLoadData () {
-        //if it is not modal form then load news and other page stuff
+
+        // If global search or new window aka modal then
+        // do not load /webpages/{id} data.
         if(!isDetailForm() && !isGlobalSearch()) {
+
+            // Gets news
             applicationService.getNews(onNewsData);
-            // content of webpages
+
             vm.geocollection = getWebPageById(2, "geocollection");
             vm.usingCollections = getWebPageById(32, "usingCollections");
             vm.database = getWebPageById(21, "database");
@@ -52,13 +56,9 @@ var constructor = function (configuration, $translate, $http, applicationService
      */
     function getWebPageById(id, page) {
         var myDataPromise = WebPagesFactory.getData(id);
-<<<<<<< HEAD
 
-        console.log(myDataPromise);
+        //console.log(myDataPromise);
 
-=======
-        console.log(myDataPromise);
->>>>>>> 854ff50a16a4d0dece1bc6b1ad9992bad4ce27b4
         myDataPromise.then(function(result) {
             if (page == "geocollection") { vm.geocollection = result; }
             else if (page == "usingCollections") { vm.usingCollections = result; }
@@ -79,6 +79,7 @@ var constructor = function (configuration, $translate, $http, applicationService
         //console.log(response);
 
         vm.news = response.data;
+        console.log(response.data);
         angular.forEach(vm.news.results, function(currentNews) {
             var year = currentNews.date_added.split("-")[0];
             if (vm.years.indexOf(year) == -1) { vm.years.push(year); }
@@ -86,37 +87,70 @@ var constructor = function (configuration, $translate, $http, applicationService
         $scope.yearToShow = vm.years[0];
     }
 
+    /**
+     * User clicks on certain year under news section
+     * then yearToShow value updates as what was
+     * clickable object's innerText
+     * @param $event Click event
+     */
     function showNews($event) {
         $scope.yearToShow = $event.target.innerText;
     }
 
-    /*
-     *
+    /**
+     * Switches years so current yearToShow equals selectedYear.
+     * @param selectedYear Given year
      */
     function switchYear(selectedYear) {
         if (selectedYear != null) { $scope.yearToShow = selectedYear; }
     }
 
+    /**
+     * Filters yearToShow to equal to array object's year
+     * @param id Identificator for array objects
+     * @returns returns boolean value
+     */
     function yearFilter(id) {
         if (id!= null) { return id.date_added.startsWith($scope.yearToShow); }
         return null;
     }
 
+    /**
+     * Gets elements title
+     * @param id Identificator for array objects
+     * @returns Returns correspondent title either estonian or english
+     */
     function getTitle(id) {
         if (id != null) { return $translate.use() === "et" ? id.title_et : id.title_en; }
         return null;
     }
 
+    /**
+     * Gets elements text
+     * @param id Identificator for array objects
+     * @returns Returns correspondent text
+     */
     function getText(id) {
         if (id != null) { return $translate.use() === "et" ? id.text_et : id.text_en; }
         return null;
     }
 
+    /**
+     * Gets elements content
+     * @param webpage Identificator for array objects
+     * @returns return correspondent content
+     */
     function getContent(webpage) {
+        console.log(webpage);
         if (webpage != null) { return $translate.use() === "et" ? webpage.content_et : webpage.content_en; }
         return null;
     }
 
+    /**
+     * Sets $translation parameter to use
+     * chosen language for application
+     * @param langKey Language short code
+     */
     function changeLanguage(langKey) {
         $translate.use(langKey);
     }
@@ -132,14 +166,34 @@ var constructor = function (configuration, $translate, $http, applicationService
             'preparations', 'photoArchives', 'soils','dois'];
         return searchStates.indexOf($state.current.name) > -1;
     }
+
+    /**
+     * Checks if user is on detail form or not.
+     * @returns boolean value
+     */
     function isDetailForm () {
         return $state.params && $state.params.id;
     }
+
+    /**
+     * Checks if is user on global search or not
+     * @returns boolean value
+     */
     function isGlobalSearch () {
         return $state.params && $state.params.query;
     }
 };
 
-constructor.$inject = ["configuration",'$translate', '$http', 'ApplicationService','$state', '$scope', '$rootScope', 'WebPagesFactory'];
+constructor.$inject =
+    [
+        'configuration',
+        '$translate',
+        '$http',
+        'ApplicationService',
+        '$state',
+        '$scope',
+        '$rootScope',
+        'WebPagesFactory'
+    ];
 
 module.controller("MainController", constructor);
